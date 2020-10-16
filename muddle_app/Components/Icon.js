0c7cd6fa7fd1303ab603a47d396ce9b55d5a9375
Sofiane BELAZOUZ <sofianebelazouz@gmail.com>
2020-10-17 01:51:22 +0200

    fixed icon component proptypes

--- muddle_app/Components/Icon.js
+++ muddle_app/Components/Icon.js
@@ -1,4 +1,5 @@
-import * as React from "react";
+import React from "react";
+import PropTypes from "prop-types";
 import { View } from "react-native";
 import Icon from "@expo/vector-icons/MaterialIcons";
 
@@ -12,12 +13,31 @@ const CustomIcon = (props) => {
           backgroundColor: `${props.bcolor}`,
           color: `${props.color}`,
           paddingLeft: props.pLeft,
-          borderRadius: props.rad,
-          padding: props.pad,
+          borderRadius: props.radius,
+          padding: props.padding,
         }}
       />
     </View>
   );
+};
+
+CustomIcon.propTypes = {
+  name: PropTypes.string.isRequired,
+  size: PropTypes.number,
+  bcolor: PropTypes.string,
+  color: PropTypes.string,
+  pLeft: PropTypes.number,
+  padding: PropTypes.number,
+  radius: PropTypes.number,
+};
+
+CustomIcon.defaultProps = {
+  size: 24,
+  bcolor: "unset",
+  color: "#000",
+  pLeft: 0,
+  padding: 0,
+  radius: 0,
 };
 
 export default CustomIcon;
